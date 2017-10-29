560c67d98f3e6777d0ee397b0e20544d665d2e19
Bryan Thornbury <brthor@users.noreply.github.com>
2017-10-29 10:09:48 -0700

    Improve logging in the task client (#2)
    * improve logging in the task client
    
    * adjust namespaces

---
+++ Trace.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Gofer.NET
+{
+    public static class Trace
+    {
+        private static readonly object _locker = new object();
+        
+        public static void Exception(string message, Exception exception)
+        {
+            lock (_locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                
+                Console.WriteLine(message);
+                Console.WriteLine(exception);
+
+                Console.ForegroundColor = oldColor;
+            }
+        }
+        
+        public static void Error(string message)
+        {
+            lock (_locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                
+                Console.WriteLine(message);
+
+                Console.ForegroundColor = oldColor;
+            }
+        }
+        
+        public static void Warning(string message)
+        {
+            lock (_locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                
+                Console.WriteLine(message);
+
+                Console.ForegroundColor = oldColor;
+            }
+        }
+        
+        public static void Info(string message)
+        {
+            lock (_locker)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                
+                Console.WriteLine(message);
+
+                Console.ForegroundColor = oldColor;
+            }
+        }
+    }
+}
