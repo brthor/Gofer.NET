bec9f02ce60967ecae5ed180973c65c65f2b3961
Bryan Thornbury <bt.mozoby@gmail.com>
2017-11-06 16:14:57 -0800

    try glob csproj

--- publish.sh
+++ publish.sh
@@ -6,6 +6,6 @@ package_name="$1"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "Publishing ${package_name} $(printf %05d $TRAVIS_BUILD_NUMBER)"
 
-    dotnet pack -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
+    dotnet pack *.csproj -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
     dotnet nuget push ./bin/Release/${package_name}*.nupkg -s https://www.nuget.org/api/v2/package -k $NUGET_KEY
 fi
