e32ed6e9754c938a8a91a536e108e482958eb1a7
Bryan Thornbury <bt.mozoby@gmail.com>
2019-06-25 11:45:31 -0700

    add osx build, update test runner

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@ set -ev
 
 package_name="$1"
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_OS_NAME" = "linux" ]; then
     echo "Publishing ${package_name} $(printf %05d $TRAVIS_BUILD_NUMBER)"
 
     dotnet pack *.csproj -c Release --version-suffix $(printf %05d $TRAVIS_BUILD_NUMBER)
