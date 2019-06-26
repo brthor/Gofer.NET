d8c9c194665a4bb35174a0476f37359b786dbd3a
Bryan Thornbury <bt.mozoby@gmail.com>
2019-06-26 14:00:33 -0700

    Add Sample Project Structure

---
+++ Documentation/example/primary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gofer.NET;
+
+namespace primary
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var redisConnectionString = "127.0.0.1:6379";
+            
+            // Create a Task Client connected to Redis
+            var taskClient = new TaskClient(TaskQueue.Redis(redisConnectionString));
+            
+            // Queue up a Sample Fire-And-Forget Job
+            await taskClient.TaskQueue.Enqueue(() => WriteValue("Hello World!"));
+
+            // Queue up a job to be run in 30 seconds
+            await taskClient.TaskScheduler
+                .AddScheduledTask(() => WriteValue("Scheduled Task!"), TimeSpan.FromSeconds(30));
+
+            // Queue up a Recurring Job for every 10 seconds
+            await taskClient.TaskScheduler
+                .AddRecurringTask(() => WriteDate(), TimeSpan.FromSeconds(10), "my-recurring-job");
+        }
+        
+        private static void WriteValue(object value)
+        {
+            Console.WriteLine(value.ToString());
+        }
+
+        private static void WriteDate()
+        {
+            Console.WriteLine(DateTime.UtcNow.ToString());
+        }
+    }
+}
---
+++ Documentation/example/worker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gofer.NET;
+
+namespace worker
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var redisConnectionString = "127.0.0.1:6379";
+            
+            // Create a Task Client connected to Redis
+            var taskClient = new TaskClient(TaskQueue.Redis(redisConnectionString));
+
+            Console.WriteLine("Listening for Jobs...");
+            
+            // Endlessly listen for jobs
+            await taskClient.Listen();
+        }
+    }
+}
